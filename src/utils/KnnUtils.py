2a56539f8003a49f047faa109217894d20abb9e3
Thiago <thiagoseronni@gmail.com>
2023-11-05 13:07:56 -0300

    update clustering deep learning

--- src/utils/KnnUtils.py
+++ src/utils/KnnUtils.py
@@ -1,5 +1,7 @@
+import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
+from sklearn.decomposition import PCA
 import seaborn as sns
 import matplotlib.pyplot as plt
 from math import sqrt
@@ -19,34 +21,30 @@ class OptimalCluster:
         self.n_cluster_min = n_cluster_min
         self.n_cluster_max = n_cluster_max
         self.random_state = random_state
+        self.all_kmeans = []
+        self.x_labels = range(self.n_cluster_min, self.n_cluster_max + 1)
 
-        sum_of_squares = self.calculate_wcss(data=self.data, x_min=self.n_cluster_min, x_max=self.n_cluster_max)
-        k_elbow_best = self.elbow_optimal_number_of_clusters(sum_of_squares)
+        for n in range(self.n_cluster_min, self.n_cluster_max + 1):
+            kmeans = KMeans(n_clusters=n, random_state=self.random_state, n_init=self.n_cluster_max)
+            kmeans.fit(X=data)
+            self.all_kmeans.append(kmeans)
 
-        k_silhouette_best, silhouette, n = self.silhouette_optimal_number_of_clusters(data=self.data, x_min=self.n_cluster_min, x_max=self.n_cluster_max)
+        self.sum_of_squares = self.calculate_wcss()
+        self.k_elbow_best, self.k_elbow_best_index = self.elbow_optimal_number_of_clusters(self.sum_of_squares)
 
-        sns.set(style='white', font_scale=1.1, rc={'figure.figsize': (10, 3)})
-        sns.barplot(x=n, y=silhouette, color='red')
-        plt.title('Silhouette coefficient')
-        plt.xlabel('Number of clusters')
-        plt.grid(True)
+        self.k_silhouette_best, self.k_silhouette_best_index, self.silhouette = self.silhouette_optimal_number_of_clusters()
 
-        sns.set(style='white', font_scale=1.1, rc={'figure.figsize': (10, 10)})
-        fig, ax = plt.subplots()
-        sns.lineplot(x=range(n_cluster_min, n_cluster_max + 1), y=sum_of_squares, marker='o')
-        plt.legend(labels=["Elbow"], bbox_to_anchor=(0.95, 0.95), loc='upper right')
-        plt.yticks(sum_of_squares)
-        ax2 = ax.twinx()
-        sns.lineplot(x=range(n_cluster_min, n_cluster_max + 1), y=silhouette, marker='o', color='red')
-        plt.legend(labels=["Silhouette"], bbox_to_anchor=(0.95, 0.9), loc='upper right')
-        plt.xticks(range(n_cluster_min, n_cluster_max + 1))
-        plt.yticks(silhouette)
-        plt.grid(True)
+        print(f'Optimal k for elbow method: {self.k_elbow_best}')
+        print(f'Optimal k for silhouette method: {self.k_silhouette_best}')
 
-        print(f'Optimal k for elbow method: {k_elbow_best}')
-        print(f'Optimal k for silhouette method: {k_silhouette_best}')
+    def get_kmeans(self, k=2):
+        return self.all_kmeans[k - self.n_cluster_min]
 
-    def calculate_wcss(self, data, x_min, x_max):
+    def plot_pca_best_distributions(self):
+        self.plot_pca_cluster_distribution(k=self.k_elbow_best)
+        self.plot_pca_cluster_distribution(k=self.k_silhouette_best)
+
+    def calculate_wcss(self):
         """
             WCSS (Within-Cluster Sum of Squares) é uma medida utilizada no algoritmo K-means para avaliar a qualidade da clusterização.
             Essa medida calcula a soma dos quadrados das distâncias entre cada amostra e o centro do cluster ao qual ela pertence.
@@ -55,16 +53,15 @@ class OptimalCluster:
         """
 
         wcss = []
-        for n in range(x_min, x_max + 1):
-            kmeans = KMeans(n_clusters=n, random_state=self.random_state, n_init=x_max)
-            kmeans.fit(X=data)
+        for n in range(0, len(self.all_kmeans)):
+            kmeans = self.all_kmeans[n]
             wcss.append(kmeans.inertia_)
 
         return wcss
 
     def elbow_optimal_number_of_clusters(self, wcss):
-        x1, y1 = 2, wcss[0]
-        x2, y2 = 20, wcss[len(wcss) - 1]
+        x1, y1 = self.n_cluster_min, wcss[0]
+        x2, y2 = self.n_cluster_max, wcss[len(wcss) - 1]
 
         distances = []
         for i in range(len(wcss)):
@@ -74,9 +71,12 @@ class OptimalCluster:
             denominator = sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2)
             distances.append(numerator / denominator)
 
-        return distances.index(max(distances)) + 2
+        k_elbow_best_index = distances.index(max(distances))
+        k_elbow_best = k_elbow_best_index + self.n_cluster_min
 
-    def silhouette_optimal_number_of_clusters(self, data, x_min, x_max):
+        return k_elbow_best, k_elbow_best_index
+
+    def silhouette_optimal_number_of_clusters(self):
         """
             O coeficiente silhuera varia de -1 a +1, em que
             +1 denota está muito coeso com seu cluster e muitos distante dos demais clusters;
@@ -86,16 +86,60 @@ class OptimalCluster:
 
         silhouette = []
 
-        n = [i for i in range(x_min, x_max + 1)]
-
-        for i in range(x_min, x_max + 1):
-            kmeans = KMeans(n_clusters=i, random_state=self.random_state, n_init=x_max)
-            kmeans.fit(data)
-            silhouette.append(silhouette_score(data,
+        for i in range(0, len(self.all_kmeans)):
+            kmeans = self.all_kmeans[i]
+            silhouette.append(silhouette_score(self.data,
                                                kmeans.labels_,
                                                metric='euclidean'))
 
-        k_silhouette_best = n[silhouette.index(max(silhouette))]
+        k_silhouette_best_index = silhouette.index(max(silhouette))
+        k_silhouette_best = k_silhouette_best_index + self.n_cluster_min
 
-        return k_silhouette_best, silhouette, n
+        return k_silhouette_best, k_silhouette_best_index, silhouette
+
+    def plot_silhouette_scores(self):
+        """Plot the silhouette scores for different numbers of clusters."""
+        sns.set(style='white', font_scale=1.1, rc={'figure.figsize': (10, 3)})
+        sns.barplot(x=self.x_labels, y=self.silhouette, color='red')
+        plt.title('Silhouette Coefficient')
+        plt.xlabel('Number of Clusters')
+        plt.ylabel('Silhouette Score')
+        plt.grid(True)
+        plt.show()
+
+    def plot_elbow_and_silhouette_lines(self):
+        """Plot the elbow and silhouette."""
+        sns.set(style='white', font_scale=1.1, rc={'figure.figsize': (10, 10)})
+        fig, ax = plt.subplots()
+        sns.lineplot(x=self.x_labels, y=self.sum_of_squares, marker='o')
+        plt.legend(labels=["Elbow"], bbox_to_anchor=(0.95, 0.95), loc='upper right')
+        plt.ylabel('WCSS')
+        ax2 = ax.twinx()
+        sns.lineplot(x=self.x_labels, y=self.silhouette, marker='o', color='red')
+        plt.legend(labels=["Silhouette"], bbox_to_anchor=(0.95, 0.9), loc='upper right')
+        plt.xticks(self.x_labels)
+        plt.yticks(self.silhouette)
+        plt.ylabel('Silhouette Score')
+        plt.grid(True)
+        plt.show()
+
+    def plot_pca_cluster_distribution(self, k=2):
+        """Plot the PCA cluster distribution for a given KMeans model."""
+        pca = PCA(n_components=2)
+        pca_result = pca.fit_transform(self.data)
+        pca_df = pd.DataFrame(pca_result, columns=['PC1', 'PC2'])
+
+        k_index = k - self.n_cluster_min
+        kmeans_model = self.all_kmeans[k_index]
+        kmeans_model.fit(pca_result)
+
+        pca_df['cluster'] = kmeans_model.labels_
+
+        plt.figure(figsize=(10, 7))
+        sns.scatterplot(x='PC1', y='PC2', hue='cluster', data=pca_df, palette='viridis')
+        plt.title(f'PCA - Cluster Distribution for K={k}')
+        plt.xlabel('Principal Component 1')
+        plt.ylabel('Principal Component 2')
+        plt.legend(title='Cluster')
+        plt.show()
 
--- src/utils/SpacyUtils.py
+++ src/utils/SpacyUtils.py
@@ -2,21 +2,25 @@ import spacy
 
 
 class SpacyUtil:
-    def __init__(self, model='en_core_web_sm'):
+    def __init__(self, model='en_core_web_sm', lemma=True, remove_stopwords=True, lower=True, remove_numbers=True):
 
         self.model_name = model
         self.nlp = spacy.load(self.model_name)
+        self.lemma = lemma
+        self.remove_stopwords = remove_stopwords
+        self.lower = lower
+        self.remove_numbers = remove_numbers
 
-    def preprocess_text(self, text='', lemma=True, remove_stopwords=True, lower=True, remove_numbers=True):
+    def preprocess_text(self, text=''):
 
         doc = self.nlp(text)
 
         tokens = []
         for token in doc:
-            if (remove_stopwords and token.is_stop) or token.is_punct or (remove_numbers and token.like_num):
+            if (self.remove_stopwords and token.is_stop) or token.is_punct or (self.remove_numbers and token.like_num):
                 continue
-            token_text = token.lemma_.lower() if lemma else token.text
-            if not lower:
+            token_text = token.lemma_.lower() if self.lemma else token.text
+            if not self.lower:
                 token_text = token_text.capitalize()
             tokens.append(token_text)
 
